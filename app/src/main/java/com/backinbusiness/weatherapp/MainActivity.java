38a7fdb5ff875b6679f613944d7e012cd4f0a3ce
Kirill Lapardin <lapardin@yanex.ru>
2019-04-23 14:13:14 +0900

    Работоа со строками

--- app/src/main/java/com/backinbusiness/weatherapp/MainActivity.java
+++ app/src/main/java/com/backinbusiness/weatherapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.backinbusiness.weatherapp;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        changeTextColor();
+    }
+
+    private void initViews(){
+        textView = findViewById(R.id.textView);
+    }
+
+    private void changeTextColor(){
+        int color = ContextCompat.getColor(getApplicationContext(),R.color.colorPrimaryDark);
+        textView.setTextColor(color);
     }
 }
